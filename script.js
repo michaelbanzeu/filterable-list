0a6a94e77a2f34d26733a321c548843af940da7e
Michael Banzeu <banzeumike@gmail.com>
2023-08-08 18:38:06 +0100

    Code updated (minor changes)

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Get input element
-let filterInput = document.getElementById('filterInput');
+const filterInput = document.getElementById('filterInput');
 // Add an event on it
 filterInput.addEventListener('keyup', filterNames);
 
 function filterNames() {
     // Get the value of the input
-    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    let filterValue = filterInput.value.toUpperCase();
     // Get the entire contact list
-    let contacts = document.getElementById('names');
+    const contacts = document.getElementById('names');
     // Get items in the contact list
-    let names = contacts.querySelectorAll('li.collection-item');
-    // Loop through the collection of names
-    for (var i = 0; i < names.length; i++) {
-        let a = names[i].getElementsByTagName('a')[0];
+    const names = contacts.querySelectorAll('li.collection-item');
+    //     // Loop through the collection of names
+    for (const name of names) {
+        let contact = name.getElementsByTagName('a')[0];
         // Check if matched
-        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
-            names[i].style.display = '';
+        if (contact.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            name.style.display = "";
         } else {
-            names[i].style.display = 'none';
+            name.style.display = "none";
         }
     }
 }
